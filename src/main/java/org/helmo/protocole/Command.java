74654d64bb7bc513877d7c63cba336478299fcee
romaincoibion <romaincoibion@gmail.com>
2024-02-14 11:50:01 +0100

    Regex primitives + Objet Command

---
+++ src/main/java/org/helmo/protocole/Command.java
@@ -0,0 +1,10 @@
+package org.helmo.protocole;
+
+public class Command {
+    private String CommandType = null;
+
+
+    public String getCommandType() {
+        return CommandType;
+    }
+}
---
+++ src/main/java/org/helmo/protocole/Protocole.java
@@ -0,0 +1,30 @@
+package org.helmo.protocole;
+
+public class Protocole {
+    // Expressions régulières réutilisées
+    private static final String letter = "[A-Za-z]";
+    private static final String digit = "[0-9]";
+    private static final String letter_digit = "(" + letter + "|" + digit + ")";
+    private static final String crlf = "\\x0D\\x0A";
+    private static final String port = digit + "{1,5}";
+    private static final String character = "[\\x20-\\xFF]";
+    private static final String character_spec = "[-_\\.=+\\*\\$°\\(\\)\\[\\]\\{\\}\\^]";
+    private static final String character_pass = "(" + letter_digit + "|" + character_spec + ")";
+    private static final String sp = "\\x20";
+    private static final String id = letter_digit + "{5,10}";
+    private static final String protocol = letter_digit + "{3,15}";
+    private static final String username = letter_digit + "{3,50}";
+    private static final String password = character_pass + "{3,50}";
+    private static final String authentication = character_pass + "{3,50}";
+    private static final String password_auth = password + "(?:#" + authentication + ")?";
+    private static final String host = "(" + letter_digit + "|\\.|_|-)" + "{3,50}";
+    private static final String path = "/(?:" + letter_digit + "|\\.|_|-|/){0,100}";
+    private static final String url = "(" + protocol + "://(?:" + username + "(?::" + password_auth + ")?@" + host + "(?::" + port + ")?(?:" + path + ")?))";
+    private static final String min = digit + "{1,8}";
+    private static final String max = digit + "{1,8}";
+    private static final String frequency = digit + "{1,8}";
+    private static final String augmented_url = id + "!" + url + "!" + min + "!" + max;
+    private static final String state = "(?:OK|ALARM|DOWN|UNKNOWN)";
+    private static final String message = character + "{1,200}";
+
+}
