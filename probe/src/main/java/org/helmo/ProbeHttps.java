2f29e1e2244238bba7b985a2fe861da27e78ce47
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2024-02-18 14:22:44 +0100

    Mise a jours de ProbeHttps et ProbeSNMP pour utiliser ConfigMonitor au lieu de serviceURL et pollingInterval.

--- probe/src/main/java/org/helmo/ProbeHttps.java
+++ probe/src/main/java/org/helmo/ProbeHttps.java
@@ -17,86 +17,85 @@ public class ProbeHttps extends Probe {
     private ServerSocket serverSocket;
     private final HttpClient client;
     private boolean running;
-    private final ConfigProbes configProbes;
     private final ScheduledExecutorService scheduler;
+    private final ConfigMonitor configMonitor;
 
-    public ProbeHttps(String servicesURL, int pollingInterval, ConfigProbes configProbes) {
-        super(servicesURL, pollingInterval);
-        this.client = HttpClient.newBuilder()
-                .version(HttpClient.Version.HTTP_2)
-                .followRedirects(HttpClient.Redirect.NORMAL)
-                .connectTimeout(Duration.ofSeconds(10))
-                .build();
+    public ProbeHttps(ConfigMonitor configMonitor, ConfigProbes configProbes) {
+        super(configProbes);
+        this.client = HttpClient.newBuilder().version(HttpClient.Version.HTTP_2).followRedirects(HttpClient.Redirect.NORMAL).connectTimeout(Duration.ofSeconds(10)).build();
         this.running = false;
-        this.configProbes = configProbes;
-        scheduler = Executors.newScheduledThreadPool(1);
+        this.configMonitor = configMonitor;
+        this.scheduler = Executors.newScheduledThreadPool(3);
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            System.out.println("Stopping the probe...");
+            stop();
+        }));
     }
+
     @Override
     public void start() {
-        System.out.println("Démarrage du probe HTTPS pour l'URL : " + servicesURL);
-        try {
-            serverSocket = new ServerSocket(configProbes.unicastPort());
-            System.out.println("Server Socket créé sur le port " + configProbes.unicastPort());
-        } catch (IOException e) {
-            System.out.println("Impossible de créer le server socket sur le port " + configProbes.unicastPort() + " : " + e.getMessage());
+        System.out.println("Starting the HTTPS probe");
+
+        if (!initializeServerSocket()) {
+            stop();
             return;
         }
+
         running = true;
-        scheduler.scheduleAtFixedRate(this::sendMulticastAnnouncement, 0, 90, TimeUnit.SECONDS);
-        // Utiliser un thread pour gérer la boucle de collecte périodique
-        new Thread(() -> {
-            while (running) {
-                collectData();
-                try {
-                    Thread.sleep(pollingInterval * 1000L);
-                } catch (InterruptedException e) {
-                    Thread.currentThread().interrupt();
-                    System.out.println("Probe interrompu");
-                }
-            }
-        }).start();
-        new Thread(this::waitForConfig).start();
+        startThreadLoop(this::sendMulticastAnnouncement, 90);
+        startThreadLoop(this::waitForConfig, 90);
+        startThreadLoop(this::collectData, Long.parseLong(configMonitor.protocolsDelay().get("https")));
+
     }
 
-    private void waitForConfig() {
-        try (Socket socket = serverSocket.accept();
-             BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
-            String configLine;
-            StringBuilder configData = new StringBuilder();
-            while ((configLine = reader.readLine()) != null) {
-                configData.append(configLine).append("\n");
-                // Traitez ici la ligne de configuration si nécessaire
-            }
-            System.out.println("Configuration reçue: " + configData);
-            // Ici, vous pouvez traiter l'ensemble de la configuration reçue
+
+    private boolean initializeServerSocket() {
+        try {
+            serverSocket = new ServerSocket(configProbes.unicastPort());
+            System.out.println("Server Socket created on port " + configProbes.unicastPort());
+            return true;
         } catch (IOException e) {
-            System.out.println("Erreur lors de la réception de la configuration: " + e.getMessage());
+            System.out.println("Unable to create the server socket on port " + configProbes.unicastPort() + ": " + e.getMessage());
+            return false;
         }
-        // Après avoir reçu la configuration, la connexion se ferme automatiquement grâce au try-with-resources
     }
+
 
 
     @Override
     public void stop() {
-        System.out.println("Arrêt du probe HTTPS pour l'URL : " + servicesURL);
         running = false;
+        scheduler.shutdownNow();
         if (serverSocket != null && !serverSocket.isClosed()) {
             try {
                 serverSocket.close();
             } catch (IOException e) {
-                System.out.println("Erreur lors de la fermeture du server socket: " + e.getMessage());
+                System.out.println("Error closing the server socket: " + e.getMessage());
+            }
+        }
+        System.out.println("Probe stopped.");
+    }
+    @Override
+    protected void collectData() {
+        for (Aurl aurl : configMonitor.probes()) {
+            if (aurl.type().contains("http")) {
+                collectData(aurl);
             }
         }
     }
 
-    @Override
-    protected void collectData() {
+    private void startThreadLoop(Runnable runnable, long delay) {
+        scheduler.scheduleWithFixedDelay(() -> {
+            if (running) {
+                runnable.run();
+            }
+        }, 0, delay, TimeUnit.SECONDS);
+    }
+
+    private void collectData(Aurl aurl) {
         try {
-            HttpRequest request = HttpRequest.newBuilder()
-                    .uri(new URI(servicesURL))
-                    .timeout(Duration.ofSeconds(5))
-                    .GET()
-                    .build();
+            HttpRequest request = HttpRequest.newBuilder().uri(new URI(aurl.url().protocol() + "://" + aurl.url().host()))//TODO changer l'URI
+                    .timeout(Duration.ofSeconds(5)).GET().build();
 
             Instant start = Instant.now(); // Marquer le début de la requête
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
@@ -105,17 +104,16 @@ public class ProbeHttps extends Probe {
             long timeElapsed = Duration.between(start, finish).toMillis(); // Calculer le temps écoulé en millisecondes
             System.out.println("Réponse reçue : " + response.statusCode() + " en " + timeElapsed + " ms");
             // Ici, vous pouvez traiter la réponse, par exemple vérifier le code de statut
+
         } catch (Exception e) {
             System.out.println("Erreur lors de la collecte des données : " + e.getMessage());
         }
-
-
     }
 
     private void sendMulticastAnnouncement() {
         try (DatagramSocket socket = new DatagramSocket()) {
             socket.setBroadcast(true);
-            String message = "Sonde HTTPS démarrée : " + servicesURL;//TODO changer le message
+            String message = "Sonde HTTPS démarrée : ";//TODO changer le message
             byte[] buffer = message.getBytes();
             InetAddress group = InetAddress.getByName(configProbes.multicastAddress());
             DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, configProbes.multicastPort());
@@ -126,17 +124,26 @@ public class ProbeHttps extends Probe {
         }
     }
 
-    public static void main(String[] args) {
-        ConfigProbes configProbes = new ConfigProbes("https","224.0.0.254",65001,"wan0",10,65002,"wan0");
-        String url = "https://www.google.com";
-        Probe probe = new ProbeHttps(url, 10, configProbes);
-        probe.start();
-
+    private void waitForConfig() {
         try {
-            Thread.sleep(60000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+            serverSocket.setSoTimeout(90000); // Attendre la connexion pendant l'intervalle de l'annonce multicast
+            System.out.println("En attente de la configuration...");
+            try (Socket socket = serverSocket.accept(); BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()))) {
+                String configLine;
+                configLine = reader.readLine();
+                System.out.println("Configuration reçue: " + configLine);
+                // Ici, vous pouvez traiter l'ensemble de la configuration reçue
+                /*
+                  Exemple de traitement de la configuration reçue
+                  1. Vérifier si la configuration est valide -> analyse.isValid(configLine)
+                  2. Liste des aurls -> analyse.getAurls(configLine)
+                 3. Metre a jour la configuration
+                 */
+            } catch (SocketTimeoutException e) {
+                System.out.println("Aucune configuration reçue dans l'intervalle actuel.");
+            }
+        } catch (IOException e) {
+            System.out.println("Erreur lors de l'attente de la configuration: " + e.getMessage());
         }
-        probe.stop();
     }
 }
