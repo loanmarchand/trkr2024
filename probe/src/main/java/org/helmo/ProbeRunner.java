688ff550b3a9a60e9ec663b5dc73f9558c6c054b
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2024-02-18 14:25:39 +0100

    Mise a jours de ProbeRunner :  - Utilisation de constante pour les urls de test  - L'initialisation et la config json se fait dans le main avant le switch pour éviter de dupliquer le code.  - Les différents services sont récupéré dans les fichiers de configs.  - runHttpsProbe & runSnmpProbe s'occupe juste de créer et éxécuter leurs probe. (Pourrai être placé dans le switch)

--- probe/src/main/java/org/helmo/ProbeRunner.java
+++ probe/src/main/java/org/helmo/ProbeRunner.java
@@ -1,7 +1,8 @@
 package org.helmo;
 
 public class ProbeRunner {
-
+    final static String CONFIG_PROBES = "json/src/main/resources/config-probes.json";
+    final static String CONFIG_MONITOR = "json/src/main/resources/config-monitor.json";
     public static void main(String[] args) {
         if (args.length == 0) {
             System.out.println("Veuillez spécifier le type de sonde à exécuter ('https' ou 'snmp').");
@@ -9,12 +10,15 @@ public class ProbeRunner {
         }
 
         String probeType = args[0].toLowerCase();
+        JsonReader jsonReader = new JsonReader();
+        ConfigProbes configProbes = jsonReader.readConfigProbe(CONFIG_PROBES);
+        ConfigMonitor configMonitor = jsonReader.readConfigMonitor(CONFIG_MONITOR);
         switch (probeType) {
             case "https":
-                runHttpsProbe();
+                runHttpsProbe(configProbes, configMonitor);
                 break;
             case "snmp":
-                runSnmpProbe();
+                runSnmpProbe(configProbes, configMonitor);
                 break;
             default:
                 System.out.println("Type de sonde non reconnu. Les options valides sont 'https' ou 'snmp'.");
@@ -22,21 +26,14 @@ public class ProbeRunner {
         }
     }
 
-    private static void runHttpsProbe() {
-        // TODO: completer la méthode runHttpsProbe
-        System.out.println("Probe HTTPS");
+
+    private static void runHttpsProbe(ConfigProbes configProbes, ConfigMonitor configMonitor) {
+        Probe probe = new ProbeHttps(configMonitor, configProbes);
+        probe.start();
     }
 
-    private static void runSnmpProbe() {
-        System.out.println("Probe SNMP");
-
-        // Probe de test
-        String servicesURL1 = "1amMemb3r0fTe4mSWILA@trkr.swilabus.com:161/1.3.6.1.4.1.2021.11.11.0";
-        String servicesURL2 = "superswila:TeamG0D$wila#iLikeGodSWILA2024@v3.swi.la:6161/1.3.6.1.4.1.2021.4.11.0";
-
-        Probe probe = new ProbeSNMP(servicesURL1, 1000);
-
-        // Démarrer le probe
+    private static void runSnmpProbe(ConfigProbes configProbes, ConfigMonitor configMonitor) {
+        Probe probe = new ProbeSNMP(configMonitor, configProbes);
         probe.start();
     }
 }
