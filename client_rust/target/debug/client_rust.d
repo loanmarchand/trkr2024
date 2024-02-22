d70685146cb2ef32ba1286276fe988a1c23bf8bc
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2024-02-22 13:38:44 +0100

    Nouveau projet Rust pour le client.

---
+++ client_rust/target/debug/client_rust.d
@@ -0,0 +1,1 @@
+C:\Users\Thibaut\IdeaProjects\trkr2024\client_rust\target\debug\client_rust.exe: C:\Users\Thibaut\IdeaProjects\trkr2024\client_rust\src\main.rs
---
+++ client_rust/target/debug/deps/client_rust.d
@@ -0,0 +1,5 @@
+C:\Users\Thibaut\IdeaProjects\trkr2024\client_rust\target\debug\deps\client_rust.exe: src\main.rs
+
+C:\Users\Thibaut\IdeaProjects\trkr2024\client_rust\target\debug\deps\client_rust.d: src\main.rs
+
+src\main.rs:
