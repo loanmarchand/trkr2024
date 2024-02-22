ab6a4f0d1e2677c7f684beaf209d404f17af608a
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2024-02-22 14:57:03 +0100

    Ajout de bordures + éléments placé au bon endroit.

--- client_rust/src/main.rs
+++ client_rust/src/main.rs
@@ -1,10 +1,9 @@
 use druid::widget::{Flex, Label, SizedBox};
-use druid::{AppLauncher, Color, Widget, WidgetExt, WindowDesc, PlatformError, WindowState};
+use druid::{AppLauncher, Widget, WidgetExt, WindowDesc, PlatformError, WindowState};
 
 fn main() -> Result<(), PlatformError> {
     let main_window = WindowDesc::new(ui_builder())
         .title("Trkr Client")
-        .window_size((800.0, 600.0))
         .set_window_state(WindowState::Maximized);
 
     AppLauncher::with_window(main_window)
@@ -15,20 +14,25 @@ fn ui_builder() -> impl Widget<()> {
     // Créer le titre
     let header = Label::new("Trkr Client")
         .padding(10.0)
+        .border(druid::theme::BORDER_LIGHT, 1.0)
         .expand_width();
 
     // Créer la section de gauche
     let left_sidebar = SizedBox::empty()
+        .border(druid::theme::BORDER_LIGHT, 1.0)
+        .expand_width()
         .expand_height();
 
     // Créer la section de droite
     let right_sidebar = SizedBox::empty()
-        .background(Color::grey(0.7))
+        .border(druid::theme::BORDER_LIGHT, 1.0)
+        .expand_width()
         .expand_height();
 
     // Créer le footer
     let footer = Label::new("Footer")
         .padding(10.0)
+        .border(druid::theme::BORDER_LIGHT, 1.0)
         .expand_width();
 
     // Assembler la mise en page principale
