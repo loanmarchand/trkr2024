2db44dfd3b8255cc324052f64704bc62390f2a81
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2024-02-22 15:06:34 +0100

    Utilisation de constante + amélioration de l'UI.

--- client_rust/src/main.rs
+++ client_rust/src/main.rs
@@ -1,5 +1,8 @@
 use druid::widget::{Flex, Label, SizedBox};
-use druid::{AppLauncher, Widget, WidgetExt, WindowDesc, PlatformError, WindowState};
+use druid::{AppLauncher, Widget, WidgetExt, WindowDesc, PlatformError, WindowState, Color, Key};
+
+// Constantes
+const BORDER_COLOR: Key<Color> =  druid::theme::BORDER_LIGHT;
 
 fn main() -> Result<(), PlatformError> {
     let main_window = WindowDesc::new(ui_builder())
@@ -11,28 +14,30 @@ fn main() -> Result<(), PlatformError> {
 }
 
 fn ui_builder() -> impl Widget<()> {
-    // Créer le titre
-    let header = Label::new("Trkr Client")
-        .padding(10.0)
-        .border(druid::theme::BORDER_LIGHT, 1.0)
+    // Créer le titre centré dans un Flex
+    let header = Flex::row()
+        .with_flex_spacer(1.0)
+        .with_child(Label::new("Trkr Client").padding(10.0))
+        .with_flex_spacer(1.0)
+        .border(BORDER_COLOR, 1.0)
         .expand_width();
 
     // Créer la section de gauche
     let left_sidebar = SizedBox::empty()
-        .border(druid::theme::BORDER_LIGHT, 1.0)
+        .border(BORDER_COLOR, 1.0)
         .expand_width()
         .expand_height();
 
     // Créer la section de droite
     let right_sidebar = SizedBox::empty()
-        .border(druid::theme::BORDER_LIGHT, 1.0)
+        .border(BORDER_COLOR, 1.0)
         .expand_width()
         .expand_height();
 
     // Créer le footer
     let footer = Label::new("Footer")
         .padding(10.0)
-        .border(druid::theme::BORDER_LIGHT, 1.0)
+        .border(BORDER_COLOR, 1.0)
         .expand_width();
 
     // Assembler la mise en page principale
@@ -48,3 +53,4 @@ fn ui_builder() -> impl Widget<()> {
 
     main_layout
 }
+
