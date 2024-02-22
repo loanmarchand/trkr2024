5fdf2ca8ec52c46bd89b6a218350fc94e213ae4e
Thibaut RODERKERKEN BOGAERT <t.roderkerkenbogaert@student.helmo.be>
2024-02-22 14:11:26 +0100

    Mise a jours du fichier .gitignore

--- client_rust/target/debug/client_rust.d
+++
@@ -1,1 +0,0 @@
-C:\Users\Thibaut\IdeaProjects\trkr2024\client_rust\target\debug\client_rust.exe: C:\Users\Thibaut\IdeaProjects\trkr2024\client_rust\src\main.rs
--- client_rust/target/debug/deps/client_rust.d
+++
@@ -1,5 +0,0 @@
-C:\Users\Thibaut\IdeaProjects\trkr2024\client_rust\target\debug\deps\client_rust.exe: src\main.rs
-
-C:\Users\Thibaut\IdeaProjects\trkr2024\client_rust\target\debug\deps\client_rust.d: src\main.rs
-
-src\main.rs:
